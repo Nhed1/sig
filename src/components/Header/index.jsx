197d587ea059cb9d1b47b9a1e2a381d4e274a08c
Leonardo Garreto <leonardogarreto@gmail.com>
2022-02-28 15:43:58 -0300

    first commit project structure created

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <strong>SIG</strong>
+    </header>
+  );
+}
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.scss";
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <header>
+          <strong>Login</strong>
+        </header>
+        <div className={styles.inputs}>
+          <div>
+            <span>E-mail</span>
+            <input type="email" placeholder="Digite seu email" />
+          </div>
+          <div>
+            <span>Senha</span>
+            <input type="password" placeholder="Digite sua senha" />
+          </div>
+          <div className={styles.buttons}>
+            <a>Esqueceu sua senha ?</a>
+            <button type="submit">Entrar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
