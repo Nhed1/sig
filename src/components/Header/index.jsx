a756c0ebbb53334573cabb221ad7ff335b0fdcc7
leonardogarreto <leonardogarreto@gmail.com>
2022-04-12 15:03:45 -0300

    added bootstrap offcanvas to sidebar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,29 @@
 import styles from "./styles.module.scss";
+import { useState } from "react";
+
+import { Offcanvas } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap"; // <-- JS File
 
 export default function Header() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <header className={styles.container}>
+      <button onClick={handleShow}>Open</button>
       <strong>SIG</strong>
+      {/* out of canvas, sidebar menu */}
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <button>Dashboad</button>
+        </Offcanvas.Body>
+      </Offcanvas>
     </header>
   );
 }
