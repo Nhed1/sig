f0bcafe71d4bafa3ec2b20a4778a6ebdff14dfc5
leonardogarreto <leonardogarreto@gmail.com>
2022-04-13 10:48:07 -0300

    remove open button of tab on login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
 import styles from "./styles.module.scss";
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import { Offcanvas } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap"; // <-- JS File
 
 export default function Header() {
+  const location = useLocation();
+  const path = location.pathname;
+
   const [show, setShow] = useState(false);
   const [activeTab, setActiveTab] = useState("dashboard");
 
@@ -19,7 +23,9 @@ export default function Header() {
 
   return (
     <header className={styles.container}>
-      <button onClick={handleShow}>Open</button>
+      <button onClick={handleShow} hidden={path === "/"}>
+        Open
+      </button>
       <strong>SIG</strong>
       {/* out of canvas, sidebar menu */}
       <Offcanvas show={show} onHide={handleClose}>
