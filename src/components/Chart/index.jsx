34483cfc467eaa2ff0beabb9dd63f8cacfebeae5
leonardogarreto <leonardogarreto@gmail.com>
2022-04-14 14:54:13 -0300

    sidebar with styles and logout on sidebar

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -1,22 +1,7 @@
-import { logout } from "../../service/firebase";
-import { useState } from "react";
-
 export default function Chart() {
-  const [loading, setLoading] = useState(false);
-
-  async function handleLogout() {
-    setLoading(true);
-    try {
-      await logout();
-    } catch {
-      alert("error!");
-    }
-    setLoading(false);
-  }
   return (
     <>
       <h1>Graficos</h1>
-      <button onClick={() => handleLogout()}>logout</button>
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,8 @@
-import styles from "./styles.module.scss";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { logout } from "../../service/firebase";
 
+import styles from "./styles.module.scss";
 import { Offcanvas } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap"; // <-- JS File
@@ -27,6 +28,18 @@ export default function Header({ activeTab, setActiveTab }) {
     handleClose();
   }
 
+  // logout
+  const [loading, setLoading] = useState(false);
+  async function handleLogout() {
+    setLoading(true);
+    try {
+      await logout();
+    } catch {
+      alert("error!");
+    }
+    setLoading(false);
+  }
+
   return (
     <header className={styles.container}>
       <button onClick={handleShow} hidden={path === "/"}>
@@ -39,21 +52,21 @@ export default function Header({ activeTab, setActiveTab }) {
           <Offcanvas.Title>Menu</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <ul
-            style={{
-              listStyleType: "none",
-              cursor: "pointer",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <button onClick={() => handleActiveTab("chart")}>Gráficos</button>
-            <button onClick={() => handleActiveTab("map")}>Mapa</button>
-            <button onClick={() => handleActiveTab("table")}>Tabela </button>
-            <button onClick={() => handleActiveTab("settings")}>
-              Configurações
-            </button>
-          </ul>
+          <div className={styles.navContainer}>
+            <div className={styles.tabsContainer}>
+              <button onClick={() => handleActiveTab("chart")}>Gráficos</button>
+              <button onClick={() => handleActiveTab("map")}>Mapa</button>
+              <button onClick={() => handleActiveTab("table")}>Tabela </button>
+              <button onClick={() => handleActiveTab("settings")}>
+                Configurações
+              </button>
+            </div>
+            <div>
+              <button onClick={() => handleLogout()} hidden={path === "/"}>
+                Terminar Sessão
+              </button>
+            </div>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </header>
