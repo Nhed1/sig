320cf9470a16e9f18095da330b7309bb866a641e
Leonardo Garreto <leonardogarreto@gmail.com>
2022-05-07 16:35:10 -0300

    feat(sidebar)

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -23,7 +23,7 @@ export default function Sidebar() {
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
             <Link to="/" className="link">
-              <li className="sidebarListItem active">
+              <li className="sidebarListItem ">
                 <LineStyle className="sidebarIcon" />
                 Dashboard
               </li>
@@ -32,6 +32,7 @@ export default function Sidebar() {
               <Timeline className="sidebarIcon" />
               Mapa
             </li>
+
             <li className="sidebarListItem">
               <TrendingUp className="sidebarIcon" />
               Gráficos
